51da21e1a6b7b9daa161a88a6679494931611b0c
Alexis <lesys.exia@gmail.com>
2020-02-18 15:12:33 +0100

    Update Case.rb

--- src/Case.rb
+++ src/Case.rb
@@ -1,4 +1,4 @@
-require_relative "Lien.rb"
+require_relative "Lien"
 
 
 # Une case du jeu avec des position ligne et colonne contenant une etiquette 
@@ -7,31 +7,31 @@ class Case
 
     # @ligne => entier correspondnant à la ligne de la case dans la grille
     # @colonne => entier correspondnant à la colonne de la case dans la grille
-    # @etiquetteCase => entier correspondnant au nombre de lien total possible sur cette case
-    # @tabVoisins => tableau(0 a 3) des cases voisines 
-    # @tabTriangle => tableau(0 a 3) contenant des booleans correspondant a la possibilitée ou non de créer un lien dans une direction
+    # @etiquetteCase => entier correspondant au nombre de liens total possibles sur cette case
+    # @tabVoisins => tableau(0 à 3) des cases voisines 
+    # @tabTriangle => tableau(0 à 3) contenant des booleans correspondant à la possibilité ou non de créer un lien dans une direction
 
-    # Methode d ’ acces en lecture de @ligne
-    attr_reader:ligne
-    # Methode d ’ acces en lecture de @colonne
-    attr_reader:colonne
-    # Methode d ’ acces en lecture de @etiquetteCase
-    attr_reader:etiquetteCase
-    # Methode d ’ acces en lecture / ecriture de @tabVoisins
-    attr_accessor:tabVoisins
-    # Methode d ’ acces en lecture / ecriture de @tabTriangle
-    attr_accessor:tabTriangle
+    # Méthode d'accès en lecture de @ligne
+    attr_reader :ligne
+    # Méthode d'accès en lecture de @colonne
+    attr_reader :colonne
+    # Méthode d'accès en lecture de @etiquetteCase
+    attr_reader :etiquetteCase
+    # Méthode d'accès en lecture / écriture de @tabVoisins
+    attr_accessor :tabVoisins
+    # Méthode d'accès en lecture / écriture de @tabTriangle
+    attr_accessor :tabTriangle
 
-    # On rend privé la methode de classe new pour forcer l'utilisation de Case.creer
-    private_class_method:new
+    # On rend privé la méthode de classe new pour forcer l'utilisation de Case.creer
+    private_class_method :new
 
     # Initialisation d'une case
     #
     # === Parametres
     #
-    # * + ligne + = > la ligne de la case dans la grille
-    # * + colonne + = > la colonne de la case dans la grille
-    # * + etiquetteCase + = > le nombre de lien total pouvant etre relié à cette case
+    # * +ligne+ => la ligne de la case dans la grille
+    # * +colonne+ => la colonne de la case dans la grille
+    # * +etiquetteCase+ => le nombre de liens total pouvant être reliés à cette case
     #
     def initialize(ligne,colonne,etiquetteCase)
         @ligne=ligne
@@ -43,32 +43,32 @@ class Case
     end
 
 
-    # Creation d'une case
+    # Création d'une case
     #
-    # === Parametres
+    # === Paramètres
     #
     # * + ligne + = > la ligne de la case dans la grille
     # * + colonne + = > la colonne de la case dans la grille
-    # * + etiquetteCase + = > le nombre de lien total pouvant etre relié à cette case
+    # * + etiquetteCase + = > le nombre de liens total pouvant être reliés à cette case
     #
     # === Retour
     #
-    # la case creer
+    # La nouvelle Case
     #
     def Case.creer(ligne,colonne,etiquetteCase)
         new(ligne,colonne,etiquetteCase)
     end
 
 
-    # compte le nb de lien attaché a une case
+    # Compte le nb de lien attachés à une case
     #
-    # === Parametres
+    # === Paramètres
     #
-    # * + tabLien + = > le tableau des liens
+    # * +tabLien+ => le tableau des liens
     #
     # === Retour
     #
-    # le nombre de lien
+    # Le nombre de liens pour cette case
     #
     def nbLienCase(tabLien)
         compteur=0
@@ -83,19 +83,15 @@ class Case
 
 
 
-    # creation d'un lien a partir de cette case dans une certaine direction
+    # Création d'un lien à partir de cette case dans une certaine direction
     #
-    # === Parametres
+    # === Paramètres
     #
-    # * + posTabTriangle + = > entier qui correspond a la direction de création du lien,0==nord,1==est,2==sud,3==ouest
-    # * + hypothese + = > boolean qui correspond a l'état du lien, si il est créer ou non lors d'une hypothese
-    # * + tabLien + = > le tableau de lien de la grille
+    # * +posTabTriangle+ => entier qui correspond à la direction de création du lien (0==nord, 1==est, 2==sud, 3==ouest)
+    # * +hypothese+ => booléen qui correspond à l'état du lien, si il est créé ou non lors d'une hypothese
+    # * +tabLien+ => le tableau de lien de la grille
     #
-    # === Retour
-    #
-    # rien
-    #
-    def creerLien(posTabTriangle,hypothese,tabLien)#ATTENTION : a gerer le croisement de lien ici avant creation.
+    def creerLien(posTabTriangle,hypothese,tabLien)#ATTENTION : à gérer le croisement de lien ici avant création.
         if(self.tabTriangle[posTabTriangle]!=false && nbLienCase(tabLien)<@etiquetteCase   )
             l=Lien.creer(self,self.tabVoisins[posTabTriangle],hypothese)
             c=0
@@ -116,7 +112,7 @@ class Case
             end
             tabLien.push(l)
         else
-            puts('impossible de creer un lien entre ' "#{@etiquetteCase}"' et ' "#{self.tabVoisins[posTabTriangle].etiquetteCase}"' deja existant')
+            puts('impossible de créer un lien entre ' "#{@etiquetteCase}"' et ' "#{self.tabVoisins[posTabTriangle].etiquetteCase}"' deja existant')
         end
     end
 
